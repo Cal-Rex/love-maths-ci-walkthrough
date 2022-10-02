1b3fd1db81221d2039d0d9364cb79f7a0ea98720
Cal-Rex <andy.gibb@healthcareskills.com>
2022-10-02 16:16:20 +0000

    set up structure for game functions in js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,47 @@
-console.log('hello!')
+//  ----needed functions---
+//runGame()
+//checkAnswer()
+//calculateCorrectAnswer()
+//incrementScore()
+//incrementWrongAnswer()
+//displayAdditionQuestion()
+//displaySubtractQuestion()
+//displayMultiplyQuestion()
+//displayDivideQuestion()
+
+function runGame() {
+
+}
+
+function checkAnswer {
+
+}
+
+function calculateCorrectAnswer {
+
+}
+
+function incrementScore {
+
+}
+
+function incrementWrongAnswer {
+
+}
+
+function displayAdditionQuestion {
+
+}
+
+function displaySubtractQuestion {
+
+}
+
+function displayMultiplyQuestion {
+
+}
+
+function displayDivideQuestion {
+    
+}
+
